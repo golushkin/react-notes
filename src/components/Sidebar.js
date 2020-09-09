baa97bde60ecd3a7698e99918d12511bd62c3f3a
Andrew <goluschkin.a@yandex.by>
2020-09-09 11:49:41 +0300

    add logic in sidebar, now it change displayed note in display_note

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,37 +1,81 @@
 import React, { Component } from 'react'
-import { Button } from '@material-ui/core'
-import { Link } from 'react-router-dom'
+import { Button, Link } from '@material-ui/core'
+import { Link as RouterLink } from 'react-router-dom'
 import { TreeItem, TreeView } from '@material-ui/lab'
 import { ExpandMore, ChevronRight } from '@material-ui/icons'
+import { connect } from 'react-redux'
+import { change_current_note } from '../store/actions/data'
 import { routes } from '../routes'
 
+const mapStateToProps = state => ({
+    currentMenu: state.currentMenu,
+    notes: state.notes
+})
+
+const mapDispatchToProps = {
+    change_current_note
+}
+
+
+function get_expand_arr(route) {
+    if (route.length === 0) {
+        return []
+    }
+    if (route.length === 1) {
+        return [route]
+    }
+
+    let split_arr = route.split('-')
+    let str_route = split_arr[0]
+    const expand_route = [str_route]
+
+    for (let index = 1; index < split_arr.length; index++) {
+        str_route = `${str_route}-${split_arr[index]}`
+        expand_route.push(str_route)
+    }
+
+    return expand_route
+}
+
 export class Sidebar extends Component {
+
+    renderTree(notes, route = '', deep = 0) {
+        return notes.map((item, i) => {
+            let route_l = "";
+            if (deep === 0) {
+                route_l = `${i}`;
+            } else {
+                route_l = `${route}-${i}`;
+            }
+            return (
+                <TreeItem key={item.id} nodeId={route_l} label={item.title}>
+                    {item.children.length > 0 ? this.renderTree(item.children, route_l, deep + 1) : null}
+                </TreeItem>
+            )
+        })
+    }
+
     render() {
+
         return (
-            <div className="sidebar">     
-            <TreeView
-                defaultCollapseIcon={<ExpandMore />}
-                defaultExpandIcon={<ChevronRight />}
-                onNodeSelect={(e, value) =>console.log(value)}
-            >
-                <TreeItem nodeId="0" label="Applications">
-                    <TreeItem nodeId="0-1" label="Calendar" />
-                    <TreeItem nodeId="0-2" label="Chrome" />
-                    <TreeItem nodeId="0-3" label="Webstorm"/>
-                </TreeItem>
-                <TreeItem nodeId="5" label="Documents">
-                    <TreeItem nodeId="10" label="OSS" />
-                    <TreeItem nodeId="6" label="Material-UI">
-                        <TreeItem nodeId="7" label="src">
-                            <TreeItem nodeId="8" label="index.js" />
-                            <TreeItem nodeId="9" label="tree-view.js" />
-                        </TreeItem>
-                    </TreeItem>
-                </TreeItem>
-            </TreeView>
-            <Link to={routes.create}><Button>+ Create Note</Button></Link>
+            <div className="sidebar">
+                <TreeView
+                    expanded={get_expand_arr(this.props.currentMenu)}
+                    defaultCollapseIcon={<ExpandMore />}
+                    defaultExpandIcon={<ChevronRight />}
+                    onNodeSelect={(e, value) => this.props.change_current_note(value)}
+                >
+                    {this.renderTree(this.props.notes)}
+                </TreeView>
+                <Button>
+                    <Link color='textPrimary' component={RouterLink} to={routes.create}>
+                        + Create Note
+                    </Link>
+                </Button>
             </div>
         );
 
     }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(Sidebar)
